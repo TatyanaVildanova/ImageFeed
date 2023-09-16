ae87004a086111ddcb8b6576c7c2ea88cd16a81f
Tatyana Vildanova <tz29@yandex.ru>
2023-09-16 20:48:47 +0300

    commit1

---
+++ Profile/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  ImageFeed
+//
+//  Created by TATIANA VILDANOVA on 16.09.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    @IBOutlet private var avatarImageView: UIImageView!
+    
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    @IBOutlet private var logoutButton: UIButton!
+    @IBAction private func didTapLogoutButton() {
+    }
+    
+}
