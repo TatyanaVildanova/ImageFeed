a1bc3a5f4bf5498a307fe7cf14813b9542643d20
Tatyana Vildanova <tz29@yandex.ru>
2023-09-17 13:46:52 +0300

    commit2

---
+++ SingleImage/SingleImageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  ImageFeed
+//
+//  Created by TATIANA VILDANOVA on 17.09.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+}
