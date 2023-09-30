0c5ce6d43c8d5dd758caf59f351512aaac075fa9
Tatyana Vildanova <tz29@yandex.ru>
2023-09-30 22:35:03 +0300

    commit1

---
+++ Auth/AuthViewController.swift
@@ -0,0 +1,11 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by TATIANA VILDANOVA on 30.09.2023.
+//
+import UIKit
+
+final class AuthViewController: UIViewController {
+
+}
---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by TATIANA VILDANOVA on 29.09.2023.
+//
+
+import Foundation
+
+let AccessKey = "<EcV2mK5dqQ-PYU8-HCITILgb_kcWZEO981PwtbJngIo>"
+let SecretKey = "<TJLy1ZShNwC2i0vGUavQUNgbJvo1JX4DGpkoTxpGs0w>"
+let RedirectURI = "<urn:ietf:wg:oauth:2.0:oob>"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
